64ddafe664efae7bf5d498871f8cd7e0bc72e9e9
Timeking23 <Danielgetahun46@gmail.com>
2024-09-15 22:34:31 -0500

    skeletons

---
+++ CPU.cpp
@@ -0,0 +1,11 @@
+//  Main RAM: 8K Byte
+//  Video RAM: 8K Byte
+//  Resolution: 160x144 (20x18 tiles)
+//  Max # of Sprites: 40
+//  Max # of Sprites/line: 10
+//  Clock speed: 4.194304 MHz
+
+struct ControlUnit
+{
+    
+};
